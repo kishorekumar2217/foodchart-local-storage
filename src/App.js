f95aec2e25f06ffc45e5e94b6650c595818b27b2
kishorekumar2217 <kishorekumar2217@gmail.com>
2020-11-18 15:32:08 +0530

    msgg

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Button,Modal,Col,Image,Row,Container,P} from 'react-bootstrap';// bootstrap import ,boot strap css is added into index.js
+import './App.css';
+class App extends React.Component {
+ 
+ 
+ render (){
+  return(
+<div >
+<Container fluid>
+<Row>
+    <Col xm={12} md={6}>  
+    <p>
+      SDSGGPKDFKJD JADHHS KSJDHASD KLJHDJD JKDHJAHD DHJHADSASD JAHDJSD </p> 
+     </Col>
+     <Col md={6} xm={12}>
+     <p>
+      SDSGGPKDFKJD JADHHS KSJDHASD KLJHDJD JKDHJAHD DHJHADSASD JAHDJSD </p> 
+
+     </Col>
+  </Row>  
+  
+    
+  
+</Container>
+</div>
+  
+  );
+}
+}
+export default App;
